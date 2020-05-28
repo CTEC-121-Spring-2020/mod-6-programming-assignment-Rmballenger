4f2d6e9777f007c3d9f2d835f02f1d198f60bf2e
Rmballenger <r.ballenger@students.clark.edu>
2020-05-27 21:05:28 -0700

    Finished Prob 3

--- Prob-3/Prob-3.py
+++ Prob-3/Prob-3.py
@@ -2,13 +2,34 @@
 #   Programming Assignment 10
 #     Prob-3.py
 
-# <YOUR NAME>
+# Robert Ballenger
 
 def main():
-    # your code here
+    sum = 0
+    
+    '''
+    x = float(input("Enter a number (Or a negitive number to exit)\n"))
+    while x >= 0:
+        sum = sum + x
+        x = float(input("Enter a number (Or a negitive number to exit)\n"))
+    print("-------------------------------------")
+    print("The sum of your numbers is", sum)
+    '''
 
-    # do not change the while loop definition below
+    # Since true is always true, this while loop will loop until the break point occurs so...
     while True:
-        # your code here
+        
+        # Starting again creating x for the number given variable. Same as above.
+        x = float(input("Enter a number (Or a negitive number to exit) "))
 
+        # Now we run an if statment to check if the number given is postive or negative.
+        if x >= 0:
+            # If postive it adds the number to the sum to create a new sum.
+            sum = sum + x
+            # Then the loop restarts since True is still True and no break has been hit.
+        else:
+            # If a negative number is given, it prints out the sum again but also breaks the loop.
+            print("-------------------------------------")
+            print("The sum of your numbers is", sum)
+            break
 main()    
